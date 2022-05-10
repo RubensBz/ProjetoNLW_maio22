8412eeec38b027ec4e31075c3c000810926cdf48
Rubens Bezerra <102124922+RubensBz@users.noreply.github.com>
2022-05-10 15:22:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,35 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  //Linha Alvo (cria uma linha imaginaria no meio da tela)
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop //topo da seção
+  const sectionHeight = section.offsetHeight //altura da seção
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop //Verificar se a linha top alcançou ou ultrapassou a linha alvo
+
+  //verificar se a linha a base esta abaixo da linha alvo
+  const sectionEndAt = sectionTop + sectionHeight
+  const sectionEndAtPassedTargetLine = sectionEndAt <= targetLine
+
+  const sectionBoudaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndAtPassedTargetLine
+
+  // essa é a forma de pegar um "objeto" no html e trazer como uma string pro Js
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoudaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
